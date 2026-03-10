aebfc7da748d0c8cbb63f39ef4f3c60a9abb1b01
akerkeles-l <akerke.les@narxoz.kz>
2026-03-10 22:44:28 +0500

    bridge skill added

--- src/com/narxoz/rpg/bridge/EffectImplementor.java
+++
@@ -1,6 +0,0 @@
-package com.narxoz.rpg.bridge;
-
-public interface EffectImplementor {
-    int computeDamage(int basePower);
-    String getEffectName();
-}
--- src/com/narxoz/rpg/bridge/Skill.java
+++ src/com/narxoz/rpg/bridge/Skill.java
@@ -1,33 +1,28 @@
 package com.narxoz.rpg.bridge;
 
-import com.narxoz.rpg.composite.CombatNode;
+public class Skill {
 
-public abstract class Skill {
-    private final String skillName;
-    private final int basePower;
-    private final EffectImplementor effect;
+    private String name;
+    private int damage;
 
-    protected Skill(String skillName, int basePower, EffectImplementor effect) {
-        this.skillName = skillName;
-        this.basePower = basePower;
-        this.effect = effect;
+    public Skill(String name, int damage) {
+        this.name = name;
+        this.damage = damage;
     }
 
-    public String getSkillName() {
-        return skillName;
+    public String getName() {
+        return name;
     }
 
-    public int getBasePower() {
-        return basePower;
+    public int getDamage() {
+        return damage;
     }
 
-    public String getEffectName() {
-        return effect.getEffectName();
+    public void setName(String name) {
+        this.name = name;
     }
 
-    protected int resolvedDamage() {
-        return effect.computeDamage(basePower);
+    public void setDamage(int damage) {
+        this.damage = damage;
     }
-
-    public abstract void cast(CombatNode target);
 }
